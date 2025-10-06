94c0651aaaba8346373d6899b2c12d1e110a2010
seriousapple <seriousapplemail@gmail.com>
2025-10-06 20:32:15 +0500

    refactor xbar hass script

--- formac/xbar/hass-temp.1m.sh
+++ formac/xbar/hass-temp.1m.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 source "/Users/yaya/00files/local/en-vars"
 
-# <xbar.title>CPU Usage</xbar.title>
-# <xbar.version>1.0</xbar.version>
-# <xbar.refresh>10s</xbar.refresh>
+BASE_URL="https://hass.${LOCALDOMAIN}"
 
-# Fetch data from the Home Assistant API
-wthr=$(curl -s -H "Authorization: Bearer ${HASST}" -H "Content-Type: application/json" https://hass.${LOCALDOMAIN}/api/states/weather.forecast_home)
+call_hass() {
+  local endpoint=$1
+  curl -s \
+    -H "Authorization: Bearer ${HASST}" \
+    -H "Content-Type: application/json" \
+    "${BASE_URL}${endpoint}"
+}
 
-temp=$(curl -s -H "Authorization: Bearer ${HASST}" -H "Content-Type: application/json" https://hass.${LOCALDOMAIN}/api/states/sensor.aqara_sensor_bedroom_temperature)
-
-humi=$(curl -s -H "Authorization: Bearer ${HASST}" -H "Content-Type: application/json" https://hass.${LOCALDOMAIN}/api/states/sensor.aqara_sensor_bedroom_humidity)
-
-
-# If the API request failed, print an error
-if [ -z "$temp" ]; then
-    echo "Error: Failed to fetch data"
-    exit 1
-fi
+wthr=$(call_hass "/api/states/weather.forecast_home")
+temp=$(call_hass "/api/states/sensor.aqara_sensor_bedroom_temperature")
+humi=$(call_hass "/api/states/sensor.aqara_sensor_bedroom_humidity")
+humiwater=$(call_hass "/api/states/sensor.smartmi_evaporative_humidifer_2_water_level")
 
 # Extract the "state" value from the JSON response
-output1=$(echo "$wthr" | grep -o '"temperature":[0-9\.]*' | head -1 | cut -d':' -f2)
-output2=$(echo "$temp" | awk -F'"state":"' '{print $2}' | awk -F'"' '{print $1}')
-output3=$(echo "$humi" | awk -F'"state":"' '{print $2}' | awk -F'"' '{print $1}')
+output1="w $(echo "$wthr" | jq -r '.attributes.temperature | floor')"
+output2="t $(echo "$temp" | jq -r '.state | split(".")[0]')"
+output3="h $(echo "$humi" | jq -r '.state | split(".")[0]')"
+output4="wl $(echo "$humiwater" | jq -r '.state | split(".")[0]')"
 
 # Print the output to xbar
-final="$(echo "w $output1" | awk -F. '{print $1}') $(echo "t $output2" | awk -F. '{print $1}') $(echo "h $output3" | awk -F. '{print $1}')"
+final="$(
+  echo -n "$output1 "
+  echo -n "$output2 "
+  echo -n "$output3 "
+  echo -n "$output4 "
+)"
 
 echo "$final"
 
