fad44a2c1261a99e8ead7f22fecebcc20e9055ec
seriousapple <seriousapplemail@gmail.com>
2025-10-12 13:18:08 +0500

    update xbar hass script

--- formac/xbar/hass-temp.1m.sh
+++ formac/xbar/hass-temp.1m.sh
@@ -12,28 +12,29 @@ call_hass() {
 }
 
 wthr=$(call_hass "/api/states/weather.forecast_home")
-temp=$(call_hass "/api/states/sensor.aqara_sensor_bedroom_temperature")
+temp_bedroom=$(call_hass "/api/states/sensor.aqara_sensor_bedroom_temperature")
+temp_kitchen=$(call_hass "/api/states/sensor.aqara_sensors_kitchen_temperature")
+temp_bathroom=$(call_hass "/api/states/sensor.aqara_sensor_bathroom_temperature")
+lx=$(call_hass "/api/states/sensor.aqara_lightsensor_bedroom_illuminance")
+phone=$(call_hass "/api/states/device_tracker.yaya_iphone")
 humi=$(call_hass "/api/states/sensor.aqara_sensor_bedroom_humidity")
 humiwater=$(call_hass "/api/states/sensor.smartmi_evaporative_humidifer_2_water_level")
 
-# Extract the "state" value from the JSON response
 wthr="$(echo "$wthr" | jq -r '.attributes.temperature | floor')"
-temp="$(echo "$temp" | jq -r '.state | split(".")[0]')"
+temp_bedroom="$(echo "$temp_bedroom" | jq -r '.state | split(".")[0]')"
+temp_kitchen="$(echo "$temp_kitchen" | jq -r '.state | split(".")[0]')"
+temp_bathroom="$(echo "$temp_bathroom" | jq -r '.state | split(".")[0]')"
+lx="$(echo "$lx" | jq -r '.state | split(".")[0]')"
+phone="$(echo "$phone" | jq -r '.state | split(".")[0]')"
 humi="$(echo "$humi" | jq -r '.state | split(".")[0]')"
 humiwater="$(echo "$humiwater" | jq -r '.state | split(".")[0]')"
 
-# Print the output to xbar
-final="$(
-  echo -n "$output1 "
-  echo -n "$output2 "
-  echo -n "$output3 "
-  echo -n "$output4 "
-)"
-
-echo "t $temp h $humi"
+echo "t $temp_bedroom h $humi"
 echo "---"
-echo "$wthr outside temp"
-echo "$temp temperature"
-echo "$humi humidity"
-echo "$humiwater humi water level"
+echo "$wthr - outside temp"
+echo "br $temp_bedroom kt $temp_kitchen bt $temp_bathroom - temperature"
+echo "$lx - light sensor"
+echo "$phone - phone status"
+echo "$humi - humidity"
+echo "$humiwater - humi water level"
 
