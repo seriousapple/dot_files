5d6a9714bd0189a847d9fa0d8935accd6c875f46
seriousapple <seriousapplemail@gmail.com>
2025-12-08 15:42:54 +0500

    update xbar script

--- formac/xbar/hass-temp.1m.sh
+++ formac/xbar/hass-temp.1m.sh
@@ -17,7 +17,9 @@ temp_kitchen=$(call_hass "/api/states/sensor.aqara_sensors_kitchen_temperature")
 temp_bathroom=$(call_hass "/api/states/sensor.aqara_sensor_bathroom_temperature")
 lx=$(call_hass "/api/states/sensor.aqara_lightsensor_bedroom_illuminance")
 phone=$(call_hass "/api/states/device_tracker.yaya_iphone")
-humi=$(call_hass "/api/states/sensor.aqara_sensor_bedroom_humidity")
+humi_bedroom=$(call_hass "/api/states/sensor.aqara_sensor_bedroom_humidity")
+humi_kitchen=$(call_hass "/api/states/sensor.aqara_sensors_kitchen_humidity")
+humi_bathroom=$(call_hass "/api/states/sensor.aqara_sensor_bathroom_humidity")
 humiwater=$(call_hass "/api/states/sensor.smartmi_evaporative_humidifer_2_water_level")
 
 wthr="$(echo "$wthr" | jq -r '.attributes.temperature | floor')"
@@ -26,15 +28,17 @@ temp_kitchen="$(echo "$temp_kitchen" | jq -r '.state | split(".")[0]')"
 temp_bathroom="$(echo "$temp_bathroom" | jq -r '.state | split(".")[0]')"
 lx="$(echo "$lx" | jq -r '.state | split(".")[0]')"
 phone="$(echo "$phone" | jq -r '.state | split(".")[0]')"
-humi="$(echo "$humi" | jq -r '.state | split(".")[0]')"
+humi_bedroom="$(echo "$humi_bedroom" | jq -r '.state | split(".")[0]')"
+humi_kitchen="$(echo "$humi_kitchen" | jq -r '.state | split(".")[0]')"
+humi_bathroom="$(echo "$humi_bathroom" | jq -r '.state | split(".")[0]')"
 humiwater="$(echo "$humiwater" | jq -r '.state | split(".")[0]')"
 
-echo "t $temp_bedroom h $humi"
+echo "t $temp_bedroom h $humi_bedroom"
 echo "---"
 echo "$wthr - outside temp"
 echo "br $temp_bedroom kt $temp_kitchen bt $temp_bathroom - temperature"
 echo "$lx - light sensor"
 echo "$phone - phone status"
-echo "$humi - humidity"
+echo "br $humi_bedroom kt $humi_kitchen bt $humi_bathroom - humidity"
 echo "$humiwater - humi water level"
 
