73fd5a1a5847c69a40ed73e079ab76c0cb7f705c
seriousapple <seriousapplemail@gmail.com>
2026-02-17 18:11:58 +0500

    add emacs config

---
+++ config/emacs/init.el
@@ -0,0 +1,54 @@
+;; -*- lexical-binding: t; -*-
+(setq inhibit-startup-message t)
+
+(setq auto-save-default nil)
+(global-display-line-numbers-mode t)
+(global-font-lock-mode 1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(set-face-attribute 'default nil :family "Fira Code" :height 180)
+
+(setq calendar-week-start-day 1)
+
+(require 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+
+(require 'package)
+
+;; GNU ELPA (built-in)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+
+;; MELPA (latest packages)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(require 'evil)
+(evil-mode 0)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20"
+     "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d"
+     default))
+ '(package-selected-packages '(evil fzf gruvbox-theme magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'gruvbox-dark-soft)
+
